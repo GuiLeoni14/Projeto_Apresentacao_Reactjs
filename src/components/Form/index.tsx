08e984cbf024cafcc005c3f1a4285a5187764a9d
GuiLeoni14 <guileone167@gmail.com>
2022-03-29 12:13:27 -0300

    ajustando form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -24,7 +24,7 @@ function Form({ setUser }: { setUser(user: TypeUser): void }) {
             <h2>
                 Exemplo de envio de <strong>formulário</strong>
             </h2>
-            <InputLabel name="name" type="text" label="Digite seu email" handleChange={handleOnChange} />
+            <InputLabel name="email" type="text" label="Digite seu email" handleChange={handleOnChange} />
             <InputLabel name="senha" type="password" label="Digite sua senha" handleChange={handleOnChange} />
             <button type="submit" disabled={isLoading}>
                 Enviar
